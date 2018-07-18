ed7ff123d9629eee9c5c26f5621cdb9f85a08841
Liorby <liorby530@gmail.com>
2018-07-18 19:21:59 +0300

    lastCommit
    loginButton and profile picture

---
+++ C18 Ex1 Rotem 204360002 Lior 305346660/C18 Ex1 Rotem 204360002 Lior 305346660/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+
+namespace C18_Ex1_Rotem_204360002_Lior_305346660
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
