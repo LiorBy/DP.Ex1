f6bc143c09d47bd37c12e68064bee975d0571af5
Liorby <liorby530@gmail.com>
2018-07-18 17:12:19 +0300

    first Commit

---
+++ C18 Ex1 Rotem 204360002 Lior 305346660/C18 Ex1 Rotem 204360002 Lior 305346660/FormApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C18_Ex1_Rotem_204360002_Lior_305346660
+{
+    public partial class FormApp : Form
+    {
+        public FormApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
